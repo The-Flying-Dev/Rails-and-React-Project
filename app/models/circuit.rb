a14951d943e379e60f021df3f6aa29b8832c89ff
The-Flying-Dev <leandranm@outlook.com>
2022-08-23 02:57:38 -0700

    model validations

--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -1,3 +1,10 @@
 class Circuit < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :circuit_length, presence: true
+  validates :number_of_laps, presence: true
+  validates :fastest_lap_time, presence: true
+  validates :weather, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   #password=, password_confirmation, authenticate
   has_secure_password
+
+  has_many :circuits
   
   validates :username, presence: true, uniqueness: true
 end
